a967e6145e86a0f36561bb27ff748381c3aff47f
Anton Palermo <ANPBusinessmail@gmail.com>
2022-11-20 23:15:41 +0800

    app: auth
    configure next-auth to store crete users data on prisma

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,13 +3,20 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+import { prisma } from '../../../libs/prismaClient'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+
 export const options: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
-  ]
+  ],
+  session: {
+    strategy: 'jwt'
+  }
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) =>
